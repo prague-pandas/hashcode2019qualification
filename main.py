80f004479a889a25dc1298007cbc46ef535b185f
Filip Bartek <filip.bartek@hotmail.com>
2019-03-01 10:39:30 +0100

    Submission 10:10 PM
    * D: 322759
    * E: 313967

--- main.py
+++ main.py
@@ -2,8 +2,11 @@
 
 import argparse
 import io
+import itertools
 import random
-import time
+
+
+salt_bits = 32
 
 
 class Photo:
@@ -13,6 +16,7 @@ class Photo:
         assert (words[0] in ['H', 'V'])
         self.vertical = words[0] == 'V'
         self.tags = set(words[2:])
+        self.salt = random.getrandbits(salt_bits)
 
     def __str__(self):
         return f'{self.i} {self.vertical} {self.tags}'
@@ -27,6 +31,7 @@ class Slide:
         self.tags = photos[0].tags
         if len(photos) > 1:
             self.tags = self.tags | photos[1].tags
+        self.salt = random.getrandbits(salt_bits)
 
     def interest(self, other):
         worst = len(self.tags & other.tags)
@@ -51,27 +56,42 @@ class Instance:
     def __str__(self):
         return str(list(map(str, self.photos)))
 
-    def solve(self):
+    def solve(self, sample_size_slides, sample_size_vertical_photos):
         slides = set()
-        cache = None
+        vertical_photos = set()
         for photo in self.photos:
             if not photo.vertical:
                 slides.add(Slide([photo]))
             else:
-                if cache is None:
-                    cache = photo
-                else:
-                    slides.add(Slide([cache, photo]))
-                    cache = None
+                vertical_photos.add(photo)
+        while len(vertical_photos) >= 2:
+            if len(vertical_photos) % 1000 == 0:
+                print(f'Vertical photos remaining: {len(vertical_photos)}')
+            photo = vertical_photos.pop()
+            assert photo.vertical
+            best_score = 0
+            best_other = None
+            sample = itertools.islice(vertical_photos, sample_size_vertical_photos)
+            #sample = random.sample(vertical_photos, min(len(vertical_photos), sample_size_vertical_photos))
+            for other in sample:
+                other_score = len(photo.tags | other.tags)
+                if other_score > best_score or best_other is None:
+                    best_score = other_score
+                    best_other = other
+            slides.add(Slide([photo, best_other]))
+            vertical_photos.remove(best_other)
         cur = slides.pop()
         slideshow = [cur]
         score_acc = 0
         while len(slides) > 0:
             if len(slides) % 1000 == 0:
-                print(len(slides))
+                print(f'Slides remaining: {len(slides)}')
             best_score = 0
             best_slide = None
-            for slide in random.sample(slides, min(len(slides), 32)):
+            sample = itertools.islice(slides, sample_size_slides)
+            #sample = random.sample(slides, min(len(slides), sample_size_slides))
+            # TODO: Stop sampling as soon as we have reached a good score.
+            for slide in sample:
                 interest = cur.interest(slide)
                 if interest > best_score or best_slide is None:
                     best_score = interest
@@ -81,6 +101,7 @@ class Instance:
             cur = best_slide
             slides.remove(best_slide)
             score_acc = score_acc + best_score
+        # TODO: Improve slideshow by hillclimbing.
         solution = Solution(self, slideshow, score_acc)
         return solution
 
@@ -125,25 +146,25 @@ class Solution:
 
 
 def main():
-    random.seed(0)
+    #random.seed(0)
 
     parser = argparse.ArgumentParser()
     parser.add_argument('instance', nargs='+', type=argparse.FileType('r', encoding='utf_8'))
+    parser.add_argument('-s', type=int, default=256)
+    parser.add_argument('-v', type=int, default=256)
     namespace = parser.parse_args()
 
-    for infile in namespace.instance:
-        print(infile.name)
-        instance = Instance(infile)
-        # print(instance)
+    while True:
+        for infile in namespace.instance:
+            print(f'Input file: {infile.name}')
+            infile.seek(0)
+            instance = Instance(infile)
 
-        solution = instance.solve()
-        # print(solution)
-        start = time.time()
-        print(solution.score)
-        print(str(time.time() - start))
+            solution = instance.solve(namespace.s, namespace.v)
+            print(f'Solution score: {solution.score}')
 
-        with open(f'{instance.name}.{solution.score}.out', 'w') as outfile:
-            solution.write(outfile)
+            with open(f'{instance.name}.{solution.score}.out', 'w') as outfile:
+                solution.write(outfile)
 
 
 if __name__ == '__main__':
